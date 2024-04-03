d653944c454f34aa44e807bdeee2765b4d6e372d
max-carver <maxevans3108@gmail.com>
2024-04-03 13:31:47 +0200

    change text size

--- src/components/LandingPageText.tsx
+++ src/components/LandingPageText.tsx
@@ -8,7 +8,7 @@ const LandingPageText = () => {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ type: "spring", duration: 0.4, stiffness: 100 }}
-      className="text-zinc-50 text-5xl text-center font-light tracking-wide leading-tight"
+      className="text-zinc-50 text-4xl text-center font-light tracking-wide leading-tight"
     >
       Welcome to Avenzu, a personalized recruitment and mentorship service
       provider.
