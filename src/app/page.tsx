ef2d797499c35ea6080d383d390b73f3c3d838fa
max-carver <maxevans3108@gmail.com>
2024-04-03 13:31:03 +0200

    add about us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Card from "@/components/Card";
 import LandingPageText from "@/components/LandingPageText";
+import Image from "next/image";
 
 export default function Home() {
   const cards = [
@@ -24,7 +25,7 @@ export default function Home() {
         <LandingPageText />
       </section>
 
-      <section className="px-8 md:px-16 lg:px-32 flex items-center gap-x-5 w-full py-8">
+      <section className="px-8 md:px-16 lg:px-32 flex items-center gap-x-5 w-full py-12">
         {cards.map((card) => (
           <Card
             title={card.title}
@@ -35,6 +36,43 @@ export default function Home() {
             linkTwoHref={card.linkTwoHref}
           />
         ))}
+      </section>
+
+      <section className="px-8 md:px-16 lg:px-32 py-12 w-full">
+        <h3 className="text-2xl font-medium">
+          Tailored solutions for your aviation needs
+        </h3>
+        <h4 className="text-xl text-zinc-600">Your success, our committment</h4>
+        <div className="flex items-stretch gap-x-6 justify-between mt-8">
+          <div className="flex flex-col gap-y-4 w-1/2">
+            <p className="text-zinc-700 text-sm">
+              We are Avenzu, a personalized recruitment and mentorship service
+              provider. Our key focus is to ensure that our staff are a perfect
+              fit for their aviation placements, thereby creating a service that
+              keeps both our client and our crew satisfied and comfortable.
+            </p>
+            <p className="text-zinc-700 text-sm">
+              We specialize in bridging the gap between high level aviation
+              staffing requirements and candidates pursuing a career within the
+              business aviation and premier airline industry. Ultimately, we
+              provide an exceptional and much needed service to both the
+              aviation key stakeholders and the individual.
+            </p>
+            <p className="text-zinc-700 text-sm">
+              We take pride in our fast, current, and easy to use online
+              platform that enables seamless candidate registration as well as
+              quick browsing for aviation companies to scan through our fully
+              vetted staffing database.
+            </p>
+          </div>
+          <Image
+            src="/gulfstream-interior.jpg"
+            alt="Gulfstream interior"
+            width={450}
+            height={700}
+            className="rounded-xl"
+          />
+        </div>
       </section>
     </main>
   );
