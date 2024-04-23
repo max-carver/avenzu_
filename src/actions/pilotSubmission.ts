89d8ade47d9aea8a1e81422f71881626082a6c08
max-carver <maxevans3108@gmail.com>
2024-04-23 10:21:01 +0200

    add nodemailer config and function

--- src/actions/pilotSubmission.ts
+++ src/actions/pilotSubmission.ts
@@ -1,13 +1,24 @@
 "use server";
 import { connectToDB } from "@/lib/db";
 import { PilotSubmissionModel } from "@/models/pilotSubmissionModel";
-
+import nodemailer from "nodemailer";
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_APIKEY,
   api_secret: process.env.CLOUDINARY_APISECRET,
+});
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "maxevans3108@gmail.com",
+    pass: process.env.APP_PASSWORD,
+  },
 });
 
 export const pilotSubmit = async (formData: FormData) => {
@@ -100,6 +111,20 @@ export const pilotSubmit = async (formData: FormData) => {
       cvUrl,
     });
     await newSubmission.save();
+    const mailOptions = {
+      from: "maxevans3108@gmail.com",
+      to: "maxevans3108@gmail.com",
+      subject: "New pilot Submission",
+      html: `<h1>Pilot Submission</h1><p>Name: ${firstName} ${lastName}<p>Email address: ${emailAddress}</p><p>Phone number: ${phoneNumber}</p><p>Experience: ${totalTime} hours</p></p><p>CV Url: ${cvUrl}</p><p>Photo URL: ${photoUrl}</p>`,
+    };
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (err) {
+        console.log(err);
+        return { error: err };
+      } else {
+        console.log(info);
+      }
+    });
     console.log(data);
     return {
       success: "Thank you for your submission! We'll be in touch shortly",
